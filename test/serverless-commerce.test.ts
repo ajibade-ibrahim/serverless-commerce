bf3fb841aae20c8853627e30c0256cc289e4c216
ajibade-ibrahim <ibrahim.ajibade@trilogy.com>
2022-12-13 08:23:24 +0100

    Initial commit

---
+++ test/serverless-commerce.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessCommerce from '../lib/serverless-commerce-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-commerce-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessCommerce.ServerlessCommerceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
